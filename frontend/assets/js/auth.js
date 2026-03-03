e8868e952dbe09e4da6cdf7b3940d861422e3777
Dark <kilometro0ec@gmail.com>
2026-03-03 10:42:18 -0500

    Update auth.js

--- frontend/assets/js/auth.js
+++ frontend/assets/js/auth.js
@@ -1,1 +1,18 @@
-
+async function login(usuario, password) {
+  const respuesta = await fetch(API_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      accion: "login",
+      usuario: usuario,
+      password: password,
+      token: API_KEY // Si activas la validación en el script
+    })
+  });
+  const data = await respuesta.json();
+  if (data.success) {
+    localStorage.setItem("userRole", data.rol);
+    localStorage.setItem("userId", data.id);
+    // Redirección según rol
+    if (data.rol === "TIENDAS") window.location.href = "views/tienda/dashboard.html";
+  }
+}
