829426d49f715b978402ed22bf509b23605e5133
Dark <kilometro0ec@gmail.com>
2026-03-04 02:13:55 -0500

    Update config.js

--- frontend/assets/js/config.js
+++ frontend/assets/js/config.js
@@ -1,5 +1,5 @@
 // frontend/assets/js/config.js
-const API_URL = "https://script.google.com/macros/s/AKfycbzileiH-CN1fIYY7J_pPQIg3YHLXy9NKtAS_ULgNT3LtrPRZHrlGo49a58w-jTzmWm2/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzkHi8gZlf4Uhb3QaDjdU8hOW9EWk9t5C5CpYAPN5R3oEiHHfCiLqbuoqXBmwUK64_z/exec";
 const API_KEY = "KM0_SECURE_TOKEN_2026_XYZ";
 
 // Exportar para que otros scripts lo vean si usas módulos (opcional)
