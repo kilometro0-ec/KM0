870bba16499a1258b23db6e18d7f0debc0f96ce5
Dark <kilometro0ec@gmail.com>
2026-03-05 00:17:14 -0500

    Create firebase.js

---
+++ frontend/assets/js/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getMessaging } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js";
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "km-0-c699a.firebaseapp.com",
+  projectId: "km-0-c699a",
+  storageBucket: "km-0-c699a.appspot.com",
+  messagingSenderId: "TU_SENDER_ID",
+  appId: "TU_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+export { messaging };
